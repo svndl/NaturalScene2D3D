c8272770d8072be2509c3f62ebf4777ad6661d27
Karen Duan <yirand@gmail.com>
2016-12-15 13:51:42 -0800

    update

--- maxDifference.m
+++ maxDifference.m
@@ -1,24 +1,33 @@
-function maxDifference
-    database = 'Live3D';
-    rca_path = rca_setPath;
-    dirResData = fullfile(rca_path.results_Data, database);
-    % describing splits
-    switch database
-        case 'Live3D'
-            how.allCnd = {'D', 'E'; 'D', 'O'; 'D', 'S'; 'E', 'D';'E', 'O'; 'E', 'S'; 'O', 'D'; 'O', 'E'; 'O', 'S'; 'S', 'D'; 'S', 'E'; 'S', 'O'};
-            how.splitBy = {'O', 'S'};
-            timeCourseLen = 660;
-            row = 3;
-            col = 4; 
-        case 'Middlebury'
-            how.allCnd = {'E', 'O'; 'E', 'S'; 'O', 'E'; 'O', 'S'; 'S', 'E'; 'S', 'O'};
-            how.splitBy = {'O', 'S'};
-            timeCourseLen = 500;
-            row = 5;
-            col = 6;           
-        otherwise
-    end
-    cl = {'r', 'g', 'b', 'k'};
+function maxDifference(nScenes)
+database = 'Middlebury';
+rca_path = rca_setPath;
+dirResData = fullfile(rca_path.results_Data, database);
+% describing splits
+switch database
+    case 'Live3D'
+        how.allCnd = {'D', 'E'; 'D', 'O'; 'D', 'S'; 'E', 'D';'E', 'O'; 'E', 'S'; 'O', 'D'; 'O', 'E'; 'O', 'S'; 'S', 'D'; 'S', 'E'; 'S', 'O'};
+        how.splitBy = {'O', 'S'};
+        timeCourseLen = 660;
+        row = 3;
+        col = 4;
+         how.nScenes = nScenes;
+    case 'Middlebury'
+        how.allCnd = {'E', 'O'; 'E', 'S'; 'O', 'E'; 'O', 'S'; 'S', 'E'; 'S', 'O'};
+        how.splitBy = {'O', 'S'};
+        how.nScenes = nScenes;
+        timeCourseLen = 500;
+        row = 5;
+        col = 6;
+    case 'Live3D_new'
+        how.allCnd = {'O', 'S'; 'S', 'O'};
+        how.splitBy = {'O', 'S'};
+        timeCourseLen = 750;
+        how.nScenes = nScenes;
+        
+        
+    otherwise
+end
+cl = {'r', 'g', 'b', 'k'};
             
     %% load/calculate the RC data on a given dataset
     
@@ -27,7 +36,13 @@ function maxDifference
     how.useSplits = [2, 4];
     how.baseline = 1;
     reuse = 1;
-    dirResFigures = fullfile(rca_path.results_Figures, database, strcat('rcaProject', how.splitBy{:}));
+   
+   if nScenes == 1
+        dirResFigures = fullfile(rca_path.results_Figures, database, strcat('rcaProject', how.splitBy{:},'_bySubjects'));
+        
+    else
+        dirResFigures = fullfile(rca_path.results_Figures, database, strcat('rcaProject', how.splitBy{:},'_byScenes'));
+    end
     
     if (~exist(dirResFigures, 'dir'))
         mkdir(dirResFigures);
@@ -56,26 +71,35 @@ function maxDiff(data1, data2, dataLabels, dirResData, timeCourseLen)
     
     catData1 = cat(3, data1{:});
     muData1 = nanmean(catData1, 3);
-    %semData1 = nanstd(catData1, [], 3)/sqrt(size(catData1, 3));   
-    semData1 = nanstd(catData1, [], 3)/sqrt(nSubj);   
+    semData1 = nanstd(catData1, [], 3)/sqrt(size(catData1, 3));   
+    %semData1 = nanstd(catData1, [], 3)/sqrt(nSubj);   
     
 
     catData2 = cat(3, data2{:});
     muData2 = nanmean(catData2, 3);
-    %semData2 = nanstd(catData2, [], 3)/sqrt(size(catData2, 3));   
-    semData2 = nanstd(catData2, [], 3)/sqrt(nSubj);   
+    semData2 = nanstd(catData2, [], 3)/sqrt(size(catData2, 3));   
+    %semData2 = nanstd(catData2, [], 3)/sqrt(nSubj);   
         
     [tmp1, tmp2, A, DD, WW] = calcMaxDiff(muData1, muData2);
     
     std_y1 = semData1*WW;
     std_y2 = semData2*WW;
     
+    timeCourse = linspace(0, timeCourseLen, size(muData1, 1));
+    
+    %%%%%%% for plotting in R
+%      dataframe2 = zeros(315,5); 
+%      dataframe2(:,1:3) = [timeCourse',tmp1(:,1),std_y1(:,1)];
+%      dataframe2(:,4:5) = [tmp2(:,1),std_y2(:,1)];
+%      csvwrite(fullfile(dirResFigures,strcat('plot_maxDiff.csv')),dataframe2)           
+%     
+    %%%%%%%%%
     hf = figure;
     fullTitle = ['Difference between ' dataLabels{1} ' ' dataLabels{2}];
     title(fullTitle);
     nc = size(tmp1, 2);
     
-    timeCourse = linspace(0, timeCourseLen, size(muData1, 1));
+   
 
     for c = 1:nc
         subplot(nc, 2, 2*c - 1);
@@ -105,15 +129,17 @@ function [y1, y2, A, DD, WW] = calcMaxDiff(d1, d2)
     c2 = cov(d2);
     cPool = (c1+c2)/2;
 
-	% compute eigenvalues
+	% compute eigenvalues 
     [W, D] = eig(c);
     
     DD = diag(D);
+       
+    
     %% keep top three max-dif filters
     nc = 3;
     %WW = W;
     
-    WW = W(:, end:-1:end - nc + 1);
+    WW = W(:, end:-1:end - nc + 1);% W is already sorted. 
     A = cPool*WW*inv(WW'*cPool*WW);  % scalp projections of max-dif filters
 
     %% project onto data
