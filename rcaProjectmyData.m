537b43774c0a380a8ddb78bb6bed7ac166ade28e
Karen Duan <yirand@users.noreply.github.com>
2016-12-15 14:28:29 -0800

    Delete rcaProjectmyData.m

--- rcaProjectmyData.m
+++
@@ -1,9 +0,0 @@
-function [muData, semData] = rcaProjectmyData(data, weights)
-    dataOut = rcaProject(data, weights);
-    catData = cat(3, dataOut{:});
-    muData = nanmean(catData, 3);
-    semData = nanstd(catData, [], 3)/(sqrt(size(catData, 3)));   
-    
-    %baselining
-    muData = muData - repmat(muData(1, :), [size(muData, 1) 1]);
-end
