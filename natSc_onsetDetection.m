f5b3cda6ea4e1b776e8aa95b0cb3c7f893a99f3b
Karen Duan <yirand@gmail.com>
2017-01-18 18:36:40 -0800

    add different cases

--- natSc_onsetDetection.m
+++ natSc_onsetDetection.m
@@ -1,8 +1,10 @@
-%% This script find the onset time of 2D 3D differences. 
+%% This script finds the onset time of 2D 3D differences. 
 % inputFor2D3DdifferenceOnsetDetection.csv is generated in r script (to be
 % added to github)
 %function: findOnsetTime, in svndle git mrC repository
 
+
+%Live3D_new
 cd ~/Dropbox/Research/4_IndividualDifferences/rcaNatScenes/results/figures/Live3D_new/rcaProjectOS_bySubjects/
 input = csvread('inputFor2D3DdifferenceOnsetDetection.csv');
 input = input(:,2:end);
@@ -11,6 +13,17 @@ input(24,:) = [repelem(-1,21),repelem(1,294)] %first 50 ms (which is first 21 ti
 
 
 
+%Live3D
+cd ~/Dropbox/Research/4_IndividualDifferences/rcaNatScenes/results/figures/Live3D/rcaProjectOS_bySubjects/
+input = csvread('inputFor2D3DdifferenceOnsetDetection.csv');
+input = input(:,2:end);
+input(27,:) = [repelem(-1,21),repelem(1,259)] %first 50 ms (which is first 21 time samples) as baseline. 
+[onsetTime,onsetIx] = findOnsetTime(input(25,:),input(26,:),input(27,:),'2.5stdThresh','allSeries',input(1:24,:))
 
-
+%Middlebury
+cd ~/Dropbox/Research/4_IndividualDifferences/rcaNatScenes/results/figures/Middlebury/rcaProjectOS_bySubjects/
+input = csvread('inputFor2D3DdifferenceOnsetDetection.csv');
+input = input(:,2:end);
+input(27,:) = [repelem(-1,21),repelem(1,189)] %first 50 ms (which is first 21 time samples) as baseline. 
+[onsetTime,onsetIx] = findOnsetTime(input(25,:),input(26,:),input(27,:),'2.5stdThresh','allSeries',input(1:24,:))
 
