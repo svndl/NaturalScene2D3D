dd24df750bc408a2319a8895d73ff5710e73c436
Yiran Duan <Yiran@DN0a229748.SUNet>
2017-02-23 12:07:19 -0800

    add function to train rca on separate conditions and plot the topography respectively

--- natSc_rcaOnSplitConditions.m
+++ natSc_rcaOnSplitConditions.m
@@ -1,7 +1,9 @@
-function natSc_rcaOnSplitConditions(nScenes)
+function natSc_rcaOnSplitConditions(database,nScenes,split)
 
 %This function produce the first 3 RC components for 2D vs 3D conditions,
 %and plot the topography.
+%If split == 1, RCA is trained on separate conditions (e.g 2D data alone or
+%3D data alone).
 %The RCA weights (W) were obtained from rca_run. The weights were used to
 %project back to each individual space for each trial. (For example, for
 %each individual, for 2D condition, there will be 240 trials. As there are
@@ -16,11 +18,15 @@ function natSc_rcaOnSplitConditions(nScenes)
 %input nScenes should be 1. It is needed here because some functions depend
 %on nScenes. Otherwise it is not meaningful.
 
-
-database = 'Middlebury';
 rca_path = rca_setPath;
-dirResData = fullfile(rca_path.results_Data, database);
-
+if split ==1
+    dirResData = fullfile(rca_path.results_Data, database,'TrainedSeparatedly');
+    dirFigFol = fullfile(rca_path.results_Figures, database,'TrainedSeparatedly');
+else
+    dirResData = fullfile(rca_path.results_Data, database,'TrainedTogether');
+    dirFigFol = fullfile(rca_path.results_Figures, database,'TrainedTogether');
+    
+end
 % describing splits
 
 switch database
@@ -57,12 +63,11 @@ how.nSplits = 4;
 how.useSplits = [2, 4];
 how.baseline = 0;
 reuse = 1;
-if nScenes == 1
-    dirResFigures = fullfile(rca_path.results_Figures, database, strcat('rcaProject', how.splitBy{:},'_bySubjects'));
+
+    dirResFigures = fullfile(dirFigFol, strcat('rcaProjectOS'));
     
-else
-    dirResFigures = fullfile(rca_path.results_Figures, database, strcat('rcaProject', how.splitBy{:},'_byScenes'));
-end
+
+    
 
 if (~exist(dirResFigures, 'dir'))
     mkdir(dirResFigures);
@@ -74,19 +79,49 @@ eegCND = natSc_getData4RCA(database, how, reuse);
 nReg = 7;
 nComp = 3;
 
-rcaFileAll = fullfile(dirResData, strcat('rcaOn', how.splitBy{:},'_bySubjects.mat'));
-if(~exist(rcaFileAll, 'file'))
-[rcaDataAll, W, A, ~, ~, ~, ~] = rcaRun(eegCND', nReg, nComp,2);
-save(rcaFileAll, 'rcaDataAll', 'W', 'A');
+if nScenes ==1
+    rcaFileAll = fullfile(dirResData, strcat('rcaOn', how.splitBy{:},'_bySubjects.mat'));
 else
-    load(rcaFileAll);
+    rcaFileAll = fullfile(dirResData, strcat('rcaOn', how.splitBy{:},'_byScenes.mat'));
 end
+
+
+
+
+if split ==0
+    
+    
+    if(~exist(rcaFileAll, 'file'))
+        [rcaDataAll, W, A, ~, ~, ~, ~] = rcaRun(eegCND', nReg, nComp);
+        save(rcaFileAll, 'W', 'A','rcaDataAll');
+    else
+        load(rcaFileAll);
+    end
+    
+    
+else
+    if(~exist(rcaFileAll, 'file'))
+        nCND = size(eegCND,2);
+        for nc = 1:nCND
+            
+            [rcaDataAll{nc}, W{nc}, A{nc}, ~, ~, ~, ~] = rcaRun(eegCND', nReg, nComp,nc);
+        end
+        save(rcaFileAll, 'W', 'A','rcaDataAll');
+        
+    else
+        load(rcaFileAll);
+    end
+    
+    
+    
+    
+end
+
 
 
 %project on conditions
 cl = {'r', 'g', 'b', 'k'};
-%catDataAll = cat(3, rcaDataAll{:});
-%muDataAll = nanmean(catDataAll, 3);
+
 
 timeCourse = linspace(0, timeCourseLen, size(eegCND{1, 1}, 1));
 
@@ -96,48 +131,92 @@ h = cell(nCnd, 1);
 %% run the projections, plot the topography
 baselineSample = round(50/timeCourseLen*length(timeCourse)); %First 50 ms as the baseline
 
-%R
-dataframe2 = zeros(length(timeCourse),5);
-%   %save mean and se to dataframe for the purpose of plotting in R.
 
 
-for c = 1:nComp
+if split ==0
     
-    subplot(nComp, 2, 2*c - 1);
-    color_idx = 1;
     
-    %R
-    startIdx = 1;
-    endIdx = length(timeCourse);
-    %R
-    
-    for cn = nCnd:-1:1
-        [muData_C, semData_C] = natSc_ProjectmyData(eegCND(:, cn), W,baselineSample);
-        %[muData_C, semData_C] = natSc_ProjectmyData(eegCND(:, cn), W);
+    for c = 1:nComp
         
-        %Rfor plotting in R
-        if cn ==1
-            dataframe2(startIdx:endIdx,1:3) = [timeCourse',muData_C(:,1),semData_C(:,1)];
+        subplot(nComp, 2, 2*(c) - 1);
+        color_idx = 1;
+        
+        dataframe2 = zeros(length(timeCourse),5);
+        for cn = nCnd:-1:1
+            [muData_C, semData_C] = natSc_ProjectmyData(eegCND(:, cn), W,baselineSample);
             
-        else
-            dataframe2(startIdx:endIdx,4:5) = [muData_C(:,1),semData_C(:,1)];
+            %Rfor plotting in R
+            
+            if cn ==1
+                dataframe2(:,1:3) = [timeCourse',muData_C(:,c),semData_C(:,c)];
+                
+            else
+                dataframe2(:,4:5) = [muData_C(:,c),semData_C(:,c)];
+            end
+            
+            
+            %R
+            
+            hs = shadedErrorBar(timeCourse, muData_C(:, c), semData_C(:, c), cl{color_idx}); hold on
+            h{cn} = hs.patch;
+            color_idx = color_idx + 1;
         end
-        %R
-        
-        hs = shadedErrorBar(timeCourse, muData_C(:, c), semData_C(:, c), cl{color_idx}); hold on
-        h{cn} = hs.patch;
-        color_idx = color_idx + 1;
+        csvwrite(fullfile(dirResFigures,strcat('plot2Dvs3DRCA_TrainedTogether_RC',num2str(c),'.csv')),dataframe2);
+        legend([h{end:-1:1}], [how.splitBy{end:-1:1}]'); hold on;
+        title(['RC' num2str(c) ' time course']);
+        subplot(nComp, 2, 2*c);
+        mrC.plotOnEgi(A(:,c)); hold on;
     end
-    legend([h{end:-1:1}], [how.splitBy{end:-1:1}]'); hold on;
-    title(['RC' num2str(c) ' time course']);
-    subplot(nComp, 2, 2*c);
-    mrC.plotOnEgi(A(:,c)); hold on;
+    
+    saveas(gcf, fullfile(dirResFigures, strcat('rcaProject_', how.splitBy{:})), 'fig');
+    close(gcf);
+    
+    
+else
+    
+    for c = 1:nComp
+        
+        hh = subplot(nComp, 3, 3*(c-1) + 1);
+        color_idx = 1;
+        
+        dataframe2 = zeros(length(timeCourse),5);
+        for cn = nCnd:-1:1
+            [muData_C, semData_C] = natSc_ProjectmyData(eegCND(:, cn), W{cn},baselineSample);
+            
+            %Rfor plotting in R
+            
+            if cn ==1
+                dataframe2(:,1:3) = [timeCourse',muData_C(:,c),semData_C(:,c)];
+                
+            else
+                dataframe2(:,4:5) = [muData_C(:,c),semData_C(:,c)];
+            end
+            
+            
+            %R
+            subplot(hh);
+            hs = shadedErrorBar(timeCourse, muData_C(:, c), semData_C(:, c), cl{color_idx}); hold on
+            h{cn} = hs.patch;
+            color_idx = color_idx + 1;
+            title(['RC' num2str(c),'Time Course']);
+            
+            subplot(nComp, 3, 3*c -2+cn );
+            mrC.plotOnEgi(A{cn}(:,c)); hold on;
+            title(['RC' num2str(c),':',num2str(cn+1),'D']);
+        end
+        csvwrite(fullfile(dirResFigures,strcat('plot2Dvs3DRCA_RC',num2str(c),'.csv')),dataframe2);
+        legend([h{end:-1:1}], [how.splitBy{end:-1:1}]'); hold on;
+        
+        
+        
+    end
+    
+    saveas(gcf, fullfile(dirResFigures, strcat('rcaProject_', how.splitBy{:})), 'fig');
+    %close(gcf);
+    
+    
+    
+    
+    
 end
-
-%R
-csvwrite(fullfile(dirResFigures,strcat('plot2Dvs3DRCA.csv')),dataframe2);
-%R
-
-saveas(gcf, fullfile(dirResFigures, strcat('rcaProject_', how.splitBy{:})), 'fig');
-close(gcf);
 end
