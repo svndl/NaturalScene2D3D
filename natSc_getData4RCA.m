43fbb4e7c4a7ceed2a1f1563aec0f87b5583b7dd
Karen Duan <yirand@gmail.com>
2017-04-01 10:04:41 -0700

    minor updates

--- natSc_getData4RCA.m
+++ natSc_getData4RCA.m
@@ -1,13 +1,13 @@
-function dataOut = natSc_getData4RCA(where, how, reuse)
+function dataOut = natSc_getData4RCA(database, how, reuse,split)
 % Function will attempt to split data or load existing split.
-
+ 
 % Take live3D_new data as an example, condition 1 is O-S, and condition 2 is
 % S-O. After reading in the raw trials, the data were automatically arranged
 % according to conditions. However, now we want to split it according to O and S.
 % Each trial has 1260 time points, they were divided into 4 segments, 315
 % time points each, either in scrambled - O - Scrambled -S, or Scrabmbled
 % -S - Scrabmbled - O. This function goes into each trial, and splits each
-% trial into 4 segments, and uses the 2nd and 4th segment to reconstruct
+% trial into 4 segments, and uses the specified segment to reconstruct
 % the data structure needed for rca_run. 
 %
 % Arguments: 
@@ -19,32 +19,31 @@ function dataOut = natSc_getData4RCA(where, how, reuse)
 % how.nSplits -- number of splits
 % how.splitBy -- cell array of labeled splits {}
 % how.nScenes -- if 1, organize data to subject X condition array cell, if >1 organize data to scenes X condition array cell
-
-    % what subset of subjects to use
-    database = where;
-    
+ 
+     
     %how describes how to split source data 
-    split = how.splitBy;
-    
-    % set up rca path
-    rca_Path = rca_setPath;
-    
+    splitcn = how.splitBy;
+     
+    % set up path
+    natSc_path = natSc_setPath(database,how);
+ 
+     
     % check if the requested data exists
     if how.nScenes == 1
-        dataOutFilename = strcat(database, 'data4RCA_', split{:},'_bySubjects', '.mat');
+        dataOutFilename = strcat(database, 'data4RCA_', splitcn{:},'_bySubjects', '.mat');
     else
-        dataOutFilename = strcat(database, 'data4RCA_', split{:}, '_byScenes','.mat');
+        dataOutFilename = strcat(database, 'data4RCA_', splitcn{:}, '_byScenes','.mat');
     end
-    dataOutLocation = fullfile(rca_Path.results_Data, database, dataOutFilename);
-    
+    dataOutLocation = fullfile(natSc_path.results_Data, dataOutFilename);
+     
     % if this is a new way of split or data needs to be recalculated
     if (~exist(dataOutLocation, 'file') || ~(reuse))
-        
-        
+         
+         
         eegSrc = ReadRawEEG(database,how);
         dataOut = natSc_SplitEEGData(eegSrc, how.useCnd, how.allCnd, how.splitBy, how.nSplits, how.useSplits, how.baseline);
         save(dataOutLocation, 'dataOut','-v7.3');
-        
+         
     else
         % file exists, load it 
         load(dataOutLocation);
