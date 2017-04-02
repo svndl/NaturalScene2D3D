1153658414241154aa69368a967c1b5cd4da1ae9
Karen Duan <yirand@gmail.com>
2017-04-02 09:35:36 -0700

    add case for single subject

--- natSc_ProjectmyData.m
+++ natSc_ProjectmyData.m
@@ -1,20 +1,21 @@
 function [muData, semData,out] = natSc_ProjectmyData(data, weights,baselineSample)
 
 %This function calculates the mean and standard error of each RC component.
-
+    
     dataOut = rcaProject(data, weights);
     
-    %commented out is the original calculation strategy written by Alexandra. 
-    %catData = cat(3, dataOut{:});
-    %muData = nanmean(catData, 3);
-    %semData = nanstd(catData, [], 3)/(sqrt(size(catData, 3))); %This sem
+    if size(data,1) == 1 %for single subject
+    catData = cat(3, dataOut{:});
+    muData = nanmean(catData, 3);
+    semData = nanstd(catData, [], 3)/(sqrt(size(catData, 3))); %This sem
     %is based on number of trials. 
-    %semData = nanstd(catData, [], 3)/(sqrt(size(dataOut, 1)));  %This sem is based on number of subjects. 
+    
+    else %for more than 1 subject. 
     musubjects = cellfun(@(x) nanmean(x,3), dataOut,'UniformOutput',false);%avergae 120 trials within each subject
     catsubjects = cat(3, musubjects{:}); %concatenate 21 subjects
     muData = nanmean(catsubjects, 3); %calculate mean based on 21 subject mean
     semData = nanstd(catsubjects, [], 3)/(sqrt(size(dataOut, 1)));%calculate standard error based on 21 subjects
-    
+    end
     
     %baselining uisng the first n=baselineSample time samples
     baseline = nanmean(muData(1:baselineSample,:),1);
